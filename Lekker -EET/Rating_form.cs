07aa6fce0c7fa61483d81d85cecc75c95b41ea07
Tracy Zonette Ruiters <Tracyzruiters@gmail.com>
2024-10-11 16:22:35 +0200

    Rating_form.cs

--- Lekker -EET/Rating_form.cs
+++ Lekker -EET/Rating_form.cs
@@ -15,5 +15,19 @@ namespace Lekker__EET
         {
             InitializeComponent();
         }
+        private void Submit_Click()
+        {
+            int rating = 0;
+
+            // Check which RadioButton is checked
+            if (rb1.Checked) rating = 1;
+            else if (rb2.Checked) rating = 2;
+            else if (rb3.Checked) rating = 3;
+            else if (rb4.Checked) rating = 4;
+            else if (rb5.Checked) rating = 5;
+
+            
+         Result.Text = "You rated this product: {rating}star(s)";
+        }
     }
 }
